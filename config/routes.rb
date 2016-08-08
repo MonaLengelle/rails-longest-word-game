9402860ad933015bc022518bf6d4a660f5fa106e
MonaLengelle <mona.lengelle@gmail.com>
2016-08-08 15:09:29 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'game', to: 'LongestWordGameController#game'
+  get 'score', to: 'LongestWordGameController#score'
+
 end
